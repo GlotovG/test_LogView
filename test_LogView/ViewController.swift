cb77c9a3fee1d9599b6c493334ee69294133c4c5
Glotov Gennady <glotovg@gmail.com>
2018-09-05 10:00:51 +0300

    Initial Commit

---
+++ test_LogView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test_LogView
+//
+//  Created by Gennadiy Glotov on 05.09.2018.
+//  Copyright © 2018 Gennadiy Glotov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
